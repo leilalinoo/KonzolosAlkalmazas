693bef62559830761d7dbc834e1052406f41896b
leilalinoo <aleila0921@gmail.com>
2023-10-09 08:33:51 +0200

    merge elotti commit

--- src/konzolosalkalmazas/Negyzet.java
+++ src/konzolosalkalmazas/Negyzet.java
@@ -1,13 +1,22 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package konzolosalkalmazas;
 
-/**
- *
- * @author papp.nikoletta
- */
 public class Negyzet {
+    private int x = 5;
+    private int y = 4;
+    private int oldalHossz = 10;
+    
+    
+    
+    public int terulet(){
+        return oldalHossz*oldalHossz;
+    }
+
+    public String getKoordinatak(){
+        return "["+x+";"+y+"]";
+    }
+
+    public int getOldalHossz() {
+        return oldalHossz;
+    }
     
 }
