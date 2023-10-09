4bd5e225b28e3e8dadaf5677897f0f146230a6a4
Wendesday36 <bertazoeanna@gmail.com>
2023-10-09 08:28:37 +0200

    negyzet kiiras

--- src/konzolosalkalmazas/KonzolosAlkalmazas.java
+++ src/konzolosalkalmazas/KonzolosAlkalmazas.java
@@ -1,20 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package konzolosalkalmazas;
 
-/**
- *
- * @author papp.nikoletta
- */
 public class KonzolosAlkalmazas {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+      inditas();
+    }
+
+    private static void inditas() {
+        Negyzet n = new Negyzet();
+        System.out.println("a négyzet területe:"+n.getATerulet());
+        System.out.println(n.toString());
     }
     
 }
--- src/konzolosalkalmazas/Negyzet.java
+++ src/konzolosalkalmazas/Negyzet.java
@@ -1,13 +1,26 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package konzolosalkalmazas;
 
-/**
- *
- * @author papp.nikoletta
- */
 public class Negyzet {
-    
+
+    int a = 5;
+    int x = 2;
+    int y = 3;
+
+    public int getATerulet() {
+        return a * a;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    @Override
+    public String toString() {
+        return "Negyzet{" + " x=" + x + ", y=" + y + '}';
+    }
+
 }
